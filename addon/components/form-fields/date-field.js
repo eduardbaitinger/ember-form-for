e6fabb839c8d365310c4bd32c8618a8bc3b31879
Oliver Thewissen <oliver.thewissen@dcon.de>
2018-02-12 19:24:00 +0100

    Removed (de)serialization from date-field.js as our model should store the raw ISO 8601 string (e.g. "2018-02-16")

--- addon/components/form-fields/date-field.js
+++ addon/components/form-fields/date-field.js
@@ -6,20 +6,4 @@ const { isEmpty } = Ember;
 
 export default TextField.extend({
   control: 'one-way-date',
-
-  serializeValue(value) {
-    if (value instanceof Date) {
-      return toDateString(value);
-    }
-
-    return value;
-  },
-
-  deserializeValue(value) {
-    if (isEmpty(value)) {
-      return null;
-    }
-
-    return new Date(value);
-  }
 });
